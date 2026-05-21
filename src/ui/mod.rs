a758175d9b33353ae01b2ee354f0f133a955dc4f
archko <archko@gmail.com>
2026-05-22 06:48:19 +0800

    删除不用的资源

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,10 +1,4 @@
-pub mod document_canvas;
-pub mod document_view;
-pub mod home_view;
 pub mod main_viewmodel;
 pub mod utils;
 
-pub use document_canvas::DocumentCanvasView;
-pub use document_view::document_view;
-pub use home_view::{AppState, ViewKind, home_view, HomeViewState, UIHistoryItem};
 pub use main_viewmodel::MainViewmodel;
